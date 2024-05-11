0092aa4e79568c3d24238b1cce943cd50d77fb26
zhehua-zhang <zhehuazhang123@gmail.com>
2024-05-11 03:40:04 -0400

    loss for downstream task

--- split.sh
+++ split.sh
@@ -3,13 +3,16 @@
 ratio=0.1
 
 models=('MVGRLg') 
-datasets=('MUTAG')
-template='singularity exec --writable-tmpfs --nv /data/zhehua/SIF/mvgrl.sif python3 ./run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --ratio RATIO_PLACEHOLDER --config_file random_config/mvgrlg'
+datasets=("github_stargazers")
+#datasets=("MUTAG" "MCF-7" "MOLT-4" "P388" "ZINC_full" "reddit_threads" "github_stargazers")
+template='singularity exec --writable-tmpfs --nv /data/zhehua/SIF/mvgrl.sif python3 ./run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/mvgrlg'
 commands=()
 
-for i in $(seq 0 $ratio 1); do
-    for dataset in "${datasets[@]}"; do
-        for model in "${models[@]}"; do
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do  
+        #for exp in $(seq 0 5); do #1 and 0.5 already tested
+        #    i=$(bc <<< "scale=6; 2^(-$exp)")
+        for i in $(seq $ratio $ratio 1); do
             command="${template/MODEL_PLACEHOLDER/$model}"
             command="${command/DATASET_PLACEHOLDER/$dataset}"
             command="${command/RATIO_PLACEHOLDER/$i}"
@@ -20,5 +23,6 @@ done
 
 for command in "${commands[@]}";do
     echo $command
+    eval $command
 done
-parallel -j 1 eval ::: "${commands[@]}"
+#parallel -j 1 eval ::: "${commands[@]}"
