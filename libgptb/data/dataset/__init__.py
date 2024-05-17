08952951c5e6c5785225297be33d199f80f9d25b
Chunlin Feng <chunlinfeng0920@gmail.com>
2024-05-17 05:54:22 -0400

    remove bug fix sgc datasc dataset

--- libgptb/data/dataset/__init__.py
+++ libgptb/data/dataset/__init__.py
@@ -1,13 +1,11 @@
 from libgptb.data.dataset.abstract_dataset import AbstractDataset
 from libgptb.data.dataset.pyg_dataset import PyGDataset
-from libgptb.data.dataset.sgc_dataset import SGCDataset
 from libgptb.data.dataset.tu_dataset_graphcl import TUDataset_graphcl
 from libgptb.data.dataset.tu_dataset_aug import TUDataset_aug
 from libgptb.data.dataset.tu_dataset import TUDataset
 __all__ = [
     "AbstractDataset",
     "PyGDataset",
-    "SGCDataset",
     "TUDataset_graphcl",
     "TUDataset_aug",
     "TUDataset"
