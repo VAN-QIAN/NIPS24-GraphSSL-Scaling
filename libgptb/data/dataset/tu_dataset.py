55ad4603f53b462d9c7f1f33f68f366c379e026b
Chunlin Feng <chunlinfeng0920@gmail.com>
2024-05-20 15:57:12 -0400

    integrate cci

--- libgptb/data/dataset/tu_dataset.py
+++ libgptb/data/dataset/tu_dataset.py
@@ -10,7 +10,9 @@ from torch_geometric.datasets import TUDataset
 from libgptb.data.dataset.abstract_dataset import AbstractDataset
 import importlib
 from torch_geometric.loader import DataLoader
-
+from torch_geometric.utils import add_self_loops, remove_self_loops, to_undirected, degree
+from collections import namedtuple, Counter
+import torch.nn.functional as F
 class TUDataset(AbstractDataset):
     def __init__(self, config):
         self.config = config
@@ -22,17 +24,78 @@ class TUDataset(AbstractDataset):
         
         self._load_data()
         self.ratio = self.config.get('ratio', 0)
-
+        config['num_feature']={
+            "input_dim": max(self.num_features, 1)
+        }
+        
+        
     def _load_data(self):
         device = torch.device('cuda')
         path = osp.join(os.getcwd(), 'raw_data')
-
+        deg4feat=False
+        print(self.datasetName)
         # orignal paper choices of datasets.
         #if self.datasetName in ['MUTAG', 'PTC_MR', 'IMDB-BINARY', 'IMDB-MULTI', 'REDDIT-BINARY', 'REDDIT-MULTI-5K']:
-        if self.datasetName in ["MUTAG", "MCF-7", "MOLT-4","P388","ZINC_full","reddit_threads"]:   
+        if self.datasetName in ["MUTAG", "MCF-7", "MOLT-4","P388","ZINC_full","reddit_threads","github_stargazers"]:   
             tu_dataset = getattr(importlib.import_module('torch_geometric.datasets'), 'TUDataset')
-        self.dataset = tu_dataset(path, name=self.datasetName, transform=T.NormalizeFeatures())
+            self.dataset = tu_dataset(path, name=self.datasetName)
+        if self.datasetName in ["ogbg-molhiv", "ogbg-molpcba", "ogbg-ppa", "ogbg-code2"]:
+            tu_dataset = getattr(importlib.import_module('ogb.graphproppred'), 'PygGraphPropPredDataset')
+            self.dataset = tu_dataset(root=path, name=self.datasetName)
+        
+        self.dataset = list(self.dataset)
+        graph = self.dataset[0]
+        if graph.x == None:
+            if graph.y and not deg4feat:
+                print("Use node label as node features")
+                feature_dim = 0
+                for g in dataset:
+                    feature_dim = max(feature_dim, int(g.y.max().item()))
+            
+                feature_dim += 1
+                for i, g in enumerate(self.dataset):
+                    node_label = g.y.view(-1)
+                    feat = F.one_hot(node_label, num_classes=int(feature_dim)).float()
+                    self.dataset[i].x = feat
+            else:
+                print("Using degree as node features")
+                feature_dim = 0
+                degrees = []
+                for g in self.dataset:
+                    feature_dim = max(feature_dim, degree(g.edge_index[0]).max().item())
+                    degrees.extend(degree(g.edge_index[0]).tolist())
+                MAX_DEGREES = 400
+
+                oversize = 0
+                for d, n in Counter(degrees).items():
+                    if d > MAX_DEGREES:
+                        oversize += n
+            # print(f"N > {MAX_DEGREES}, #NUM: {oversize}, ratio: {oversize/sum(degrees):.8f}")
+                feature_dim = min(feature_dim, MAX_DEGREES)
+
+                feature_dim += 1
+                for i, g in enumerate(self.dataset):
+                    degrees = degree(g.edge_index[0])
+                    degrees[degrees > MAX_DEGREES] = MAX_DEGREES
+                    degrees = torch.Tensor([int(x) for x in degrees.numpy().tolist()])
+                    feat = F.one_hot(degrees.to(torch.long), num_classes=int(feature_dim)).float()
+                    g.x = feat
+                    self.dataset[i] = g
+
+        else:
+            print("******** Use `attr` as node features ********")
+        feature_dim = int(graph.num_features)
+
+        labels = torch.tensor([x.y for x in self.dataset])
     
+        num_classes = torch.max(labels).item() + 1
+        for i, g in enumerate(self.dataset):
+            self.dataset[i].edge_index = remove_self_loops(self.dataset[i].edge_index)[0]
+            self.dataset[i].edge_index = add_self_loops(self.dataset[i].edge_index)[0]
+        #dataset = [(g, g.y) for g in dataset]
+        self.num_features=feature_dim
+        self.num_classes=num_classes
+        
     def get_data(self):
         
         assert self.train_ratio + self.test_ratio + self.test_ratio <= 1
@@ -64,7 +127,7 @@ class TUDataset(AbstractDataset):
         """
         
         return {
-            "input_dim": max(self.dataset.num_features, 1)
+            "input_dim": max(self.num_features, 1)
         }
 
 
--- libgptb/model/__init__.py
+++ libgptb/model/__init__.py
@@ -1,27 +1,6 @@
-from libgptb.model.DGI import DGI
-from libgptb.model.SUGRL import SUGRL
-from libgptb.model.CCA import CCA
-from libgptb.model.BGRL import BGRL
-from libgptb.model.SFA import SFA
-from libgptb.model.GBT import GBT
-from libgptb.model.GRACE import GRACE
-from libgptb.model.MVGRL import MVGRL
-from libgptb.model.COSTA import COSTA
-from libgptb.model.HomoGCL import HomoGCL
-from libgptb.model.MVGRLg import MVGRLg
 
-
+from libgptb.model.GraphMAE import GraphMAE
 __all__ = [
-    DGI,
-    CCA,
-    BGRL,
-    SFA,
-    SUGRL,
-    GBT,
-    GRACE,
-    MVGRL,
-    COSTA,
-    HomoGCL,
-    MVGRLg,
-    MVGRLg
+ 
+    GraphMAE
 ]
