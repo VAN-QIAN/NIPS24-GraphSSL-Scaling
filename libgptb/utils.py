ed840e3cb9634462db9ab6df7d57b25bdbbe641f
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-20 05:05:00 -0400

    delete dgl

--- libgptb/utils.py
+++ libgptb/utils.py
@@ -1,7 +1,7 @@
 from typing import *
 import os
 import torch
-import dgl
+
 import random
 import numpy as np
 
@@ -269,9 +269,6 @@ def normalize(s):
     return (s.max() - s) / (s.max() - s.mean())
 
 
-def build_dgl_graph(edge_index: torch.Tensor) -> dgl.DGLGraph:
-    row, col = edge_index
-    return dgl.graph((row, col))
 
 
 def batchify_dict(dicts: List[dict], aggr_func=lambda x: x):
