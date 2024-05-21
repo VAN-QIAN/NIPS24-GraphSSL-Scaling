1f1b6468c38bb9c21ae992200680ceead3b73beb
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-21 11:18:19 -0400

    Update split3.sh

--- split3.sh
+++ split3.sh
@@ -3,13 +3,14 @@
 ratio=("1" "0.9" "0.8" "0.7" "0.6" "0.5" "0.4" "0.3" "0.2" "0.1")
 
 models=('GraphMAE')
-datasets=( "reddit_threads")
-template='singularity exec --nv /data/qianMa/SIF/bgpmv116.sif  python3 ./run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --config_file random_config/mvgrlg'
+datasets=("reddit_threads")
+#datasets=("MUTAG" "MCF-7" "MOLT-4" "P388" "ZINC_full" "reddit_threads" "github_stargazers")
+template="python3 /data-valuation/run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/config_1"
 commands=()
 
-for i in ${ratio[@]}; do
+for model in "${models[@]}"; do
     for dataset in "${datasets[@]}"; do
-        for model in "${models[@]}"; do
+        for i in $(seq $ratio $ratio 1); do
             command="${template/MODEL_PLACEHOLDER/$model}"
             command="${command/DATASET_PLACEHOLDER/$dataset}"
             command="${command/RATIO_PLACEHOLDER/$i}"
@@ -18,7 +19,7 @@ for i in ${ratio[@]}; do
     done
 done
 
-for command in "${commands[@]}";do
+for command in "${commands[@]}"; do
     echo $command
+    eval $command
 done
-parallel -j 1 eval ::: "${commands[@]}"
