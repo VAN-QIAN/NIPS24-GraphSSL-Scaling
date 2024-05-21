351cf211ae4cc6a31fd35426071b45036a38886b
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-21 11:17:25 -0400

    Update split2.sh

--- split2.sh
+++ split2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ratio=( "0.1")
+ratio=("1" "0.9" "0.8" "0.7" "0.6" "0.5" "0.4" "0.3" "0.2" "0.1")
 
 models=('GraphMAE')
-datasets=("MUTAG")
+datasets=("reddit_threads")
 #datasets=("MUTAG" "MCF-7" "MOLT-4" "P388" "ZINC_full" "reddit_threads" "github_stargazers")
 template="python3 /data-valuation/run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/config_1"
 commands=()
