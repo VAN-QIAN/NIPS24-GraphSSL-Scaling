c4dfb84149a06db4c3849e0a17e8f1d058ec0f31
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-23 00:31:19 -0400

    s

--- split_cci.sh
+++ split_cci.sh
@@ -5,7 +5,7 @@ ratio=("1" "0.9" "0.8" "0.7" "0.6" "0.5" "0.4" "0.3" "0.2" "0.1")
 models=('GraphMAE')
 datasets=("reddit_threads" "github_stargazers")
 #datasets=("MUTAG" "MCF-7" "MOLT-4" "P388" "ZINC_full" "reddit_threads" "github_stargazers")
-template="python3 /KDD24-BGPM/run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/config_1"
+template="python3 /data-valuation/run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/config_1"
 commands=()
 
 for model in "${models[@]}"; do
