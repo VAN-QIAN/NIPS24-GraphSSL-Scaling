0ca5c1b2c953dc702c821ef6b3855b1d2c879230
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-23 19:01:11 -0400

    Update split4.sh

--- split4.sh
+++ split4.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-
-ratio=("1" "0.9" "0.8" "0.7" "0.6" "0.5" "0.4" "0.3" "0.2" "0.1")
-
-models=('GraphMAE')
-datasets=("github_stargazers")
-#datasets=("MUTAG" "MCF-7" "MOLT-4" "P388" "ZINC_full" "reddit_threads" "github_stargazers")
-template="python3 /data-valuation/run_model.py --task SSGCL --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/config_1"
+models=("34542"	"25980"	"61008"	"46587"	"89759"	"67451")
+template='singularity exec --writable-tmpfs --nv /data/zhehua/SIF/mvgrl.sif python3 ./run_model.py --task SSGCL --model MVGRLg --dataset ZINC_full --exp_id EVAL_PLACEHOLDER --train False --downstream_ratio 0.03125 --downstream_task loss --config_file random_config/mvgrlg'
 commands=()
 
 for model in "${models[@]}"; do
-    for dataset in "${datasets[@]}"; do
-        for i in $(seq $ratio $ratio 1); do
-            command="${template/MODEL_PLACEHOLDER/$model}"
-            command="${command/DATASET_PLACEHOLDER/$dataset}"
-            command="${command/RATIO_PLACEHOLDER/$i}"
+            command="${template/EVAL_PLACEHOLDER/$model}"
             commands+=("$command")
-        done
-    done
 done
 
-for command in "${commands[@]}"; do
+for command in "${commands[@]}";do
     echo $command
     eval $command
 done
+#parallel -j 1 eval ::: "${commands[@]}"
