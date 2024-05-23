0835885f0e493e23eb8242ca122ac3db70cc26fc
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-23 16:51:17 -0400

    Update split_cci.sh

--- split_cci.sh
+++ split_cci.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ratio=("1" "0.9" "0.8" "0.7" "0.6" "0.5" "0.4" "0.3" "0.2" "0.1")
+ratio=( "0.1")
 
 models=('GraphMAE')
-datasets=("github_stargazers")
+datasets=("MUTAG")
 #datasets=("MUTAG" "MCF-7" "MOLT-4" "P388" "ZINC_full" "reddit_threads" "github_stargazers")
 template="python3 ./run_model.py --task SSGCL --gpu_id 0 --model MODEL_PLACEHOLDER --dataset DATASET_PLACEHOLDER --train_ratio RATIO_PLACEHOLDER --downstream_ratio 0.1 --downstream_task loss --config_file random_config/config_1"
 commands=()
