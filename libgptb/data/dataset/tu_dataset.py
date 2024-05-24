e91266bdc28655cd27c8221f1d975fe19ec93d9a
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-23 21:24:14 -0400

    Update tu_dataset.py

--- libgptb/data/dataset/tu_dataset.py
+++ libgptb/data/dataset/tu_dataset.py
@@ -46,9 +46,6 @@ class TUDataset(AbstractDataset):
         
         self.dataset = list(self.dataset)
         graph = self.dataset[0]
-        print(graph.y)
-        y = column_or_1d(graph.y, warn=True).ravel()
-        print(y)
         print(graph)
         if graph.x == None:
             if graph.y and not deg4feat:
