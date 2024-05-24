0943e2c89cf3053f90069e3b223262160daae521
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-05-23 21:37:48 -0400

    Update __init__.py

--- libgptb/evaluators/__init__.py
+++ libgptb/evaluators/__init__.py
@@ -4,6 +4,7 @@ from libgptb.evaluators.svm import SVMEvaluator
 from libgptb.evaluators.random_forest import RFEvaluator
 from libgptb.evaluators.base_evaluator import BaseEvaluator
 from libgptb.evaluators.base_SKLearn_evluator import BaseSKLearnEvaluator
+from libgptb.evaluators.pytorch_evaluator import PyTorchEvaluator
 
 __all__ = [
     'BaseEvaluator',
@@ -12,7 +13,8 @@ __all__ = [
     'SVMEvaluator',
     'RFEvaluator',
     'get_split',
-    'from_predefined_split'
+     'from_predefined_split',
+    'PyTorchEvaluator'
 ]
 
 classes = __all__
