10af297a32bd4f17313c5ac217afdef0148c4ef0
VanQianMa <vanqianma@qq.com>
2024-05-23 21:09:52 -0400

    feat: Add PyTorchEvaluator

--- libgptb/evaluators/__init__.py
+++ libgptb/evaluators/__init__.py
@@ -5,6 +5,7 @@ from libgptb.evaluators.roc_auc import RocAucEvaluator
 from libgptb.evaluators.random_forest import RFEvaluator
 from libgptb.evaluators.base_evaluator import BaseEvaluator
 from libgptb.evaluators.base_SKLearn_evluator import BaseSKLearnEvaluator
+from libgptb.evaluators.pytorch_evaluator import PyTorchEvaluator
 
 __all__ = [
     'BaseEvaluator',
@@ -14,7 +15,8 @@ __all__ = [
     'RocAucEvaluator',
     'RFEvaluator',
     'get_split',
-    'from_predefined_split'
+    'from_predefined_split',
+    'PyTorchEvaluator'
 ]
 
 classes = __all__
