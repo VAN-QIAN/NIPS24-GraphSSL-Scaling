9dfa1c6f9155c3b217451de80e9d4f0280cde93b
Ma,Qian <maq5@rpi.edu>
2024-06-14 10:55:42 -0400

    remove DGL

--- libgptb/data/dataset/__init__.py
+++ libgptb/data/dataset/__init__.py
@@ -1,11 +1,9 @@
 from libgptb.data.dataset.abstract_dataset import AbstractDataset
 from libgptb.data.dataset.pyg_dataset import PyGDataset
-from libgptb.data.dataset.dgl_dataset import DGLDataset
 from libgptb.data.dataset.mae_dataset import MAEDataset
 
 __all__ = [
     "AbstractDataset",
     "PyGDataset",
-    "DGLDataset",
     "MAEDataset"
 ]
--- libgptb/model/__init__.py
+++ libgptb/model/__init__.py
@@ -1,14 +1,4 @@
-from libgptb.model.DGI import DGI
-from libgptb.model.SUGRL import SUGRL
-from libgptb.model.CCA import CCA
-from libgptb.model.BGRL import BGRL
-from libgptb.model.SFA import SFA
-from libgptb.model.GBT import GBT
-from libgptb.model.GRACE import GRACE
-from libgptb.model.MVGRL import MVGRL
-from libgptb.model.COSTA import COSTA
-from libgptb.model.HomoGCL import HomoGCL
-from libgptb.model.GIN import GIN
+
 from libgptb.model.GraphCL import GraphCL
 from libgptb.model.MVGRLg import MVGRLg
 from libgptb.model.InfoGraph import InfoGraph
@@ -17,20 +7,9 @@ from libgptb.model.JOAO import JOAO
 
 
 __all__ = [
-    DGI,
-    CCA,
-    BGRL,
-    SFA,
-    SUGRL,
-    GBT,
-    GRACE,
-    MVGRL,
-    COSTA,
-    HomoGCL,
-    GIN,
     GraphCL,
     MVGRLg,
     InfoGraph,
-    GraphMAE
+    GraphMAE,
     JOAO
 ]
